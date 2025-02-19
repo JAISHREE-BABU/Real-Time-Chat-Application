508441e890307a2157f84be62f56562426bd3439
JAISHREE-BABU <jaishreebabu6@gmail.com>
2025-02-19 22:41:39 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:5000");
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessages((prev) => [...prev, data]);
+    });
+
+    return () => {
+      socket.off("receive_message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (message.trim()) {
+      socket.emit("send_message", message);
+      setMessage("");
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-box">
+        {messages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Type a message..."
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+}
+
+export default App;
